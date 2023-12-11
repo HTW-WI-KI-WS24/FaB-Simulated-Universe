b82984d5ac74040e2f9974fa9c3fcb689eb8d287
nicomarantos <85935247+nicomarantos@users.noreply.github.com>
2023-12-11 15:37:25 +0100

    scraper
    - fix update_heroes.py
    - testing correct output

--- services/personas/scripts/update_heroes.py
+++ services/personas/scripts/update_heroes.py
@@ -6,33 +6,37 @@ base_url = "https://fabtcg.com/heroes"
 
 
 def scrape_heroes(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0 ...'}
 
     try:
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
         heroes = []
 
-        for hero in soup.find_all("div", class_="hero-row-name text-center py-2 mt-3 mb-5 mb-lg-3 line-before-g "
-                                                "line-after-g-dbl "):
-            name = hero.find("h2", class_="p-0 m-0 ").text
-            designation = hero.find("h5", class_="p-0 m-0").text
-            detail_link = hero.find("a")["href"]
+        # Suche nach allen 'hero-row-name' divs
+        for hero_div in soup.find_all("div", class_="hero-row-name"):
+            hero_link = hero_div.find("a")
+            if not hero_link:
+                continue
+
+            name = hero_link.find("h2").text.strip() if hero_link.find("h2") else "Unbekannter Name"
+            designation = hero_link.find("h5").text.strip() if hero_link.find("h5") else "Unbekannte Bezeichnung"
+
+            detail_link = hero_link["href"]
             full_link = urljoin(base_url, detail_link)
 
             # Scraping der Detailseite des Helden
             detail_response = requests.get(full_link, headers=headers)
             detail_soup = BeautifulSoup(detail_response.content, 'html.parser')
-            hero_text = detail_soup.find("div", class_="block-para pt-5 pb-4 container text-center").text
+            hero_text = detail_soup.find("div",
+                                         class_="block-para pt-5 pb-4 container text-center").text.strip() if detail_soup.find(
+                "div", class_="block-para pt-5 pb-4 container text-center") else "Kein Text gefunden"
 
             heroes.append({
                 "name": name,
                 "designation": designation,
                 "text": hero_text
             })
-            print(heroes)
 
         return heroes
 
@@ -42,3 +46,9 @@ def scrape_heroes(url):
 
 
 heroes_data = scrape_heroes(base_url)
+
+# Drucken der gesammelten Daten
+# for hero in heroes_data:
+# print(f"Name: {hero['name']}")
+# print(f"Designation: {hero['designation']}")
+# print(f"Text: {hero['text']}\n")
