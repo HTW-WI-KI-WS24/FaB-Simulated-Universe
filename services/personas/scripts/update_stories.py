33e45379a3b8caab9e7f614b094381135c2c1228
SpiegelV <leon.schuessler@freenet.de>
2023-12-12 03:12:09 +0100

    Fix Story-Scraper for all Links
    Fixes the scraping process in the stories scraper so that the 3 different kinds of Story Links and their respective HTML Elements get correctly scraped.

--- services/personas/scripts/update_stories.py
+++ services/personas/scripts/update_stories.py
@@ -7,19 +7,40 @@ stories_path = "stories"
 
 
 # TODO:
-#  einige stories leiten einen auf eine Domain weiter mit /heroes/(charactername)/?stories=True und nicht auf
-#  /stories. Dadurch wird bei den stories mit /heroes kein Text hinzugefügt, da man mit der normalen methode gar
-#  nicht erst darauf zugreifen kann. Letztes TODO ist eine exception erfolgreich einzubauen, die auch das abdeckt
+#  Zu den Stories zugehörige Helden mitscrapen bzw. schauen inwieweit das möglich ist
+def scrape_story_text(detail_soup):
+    story_text = ""
+
+    # Überprüfen Sie zuerst die ursprüngliche Struktur
+    story_blocks = detail_soup.find_all("div", class_="block-paragraphs rich-text")
+    if story_blocks:
+        for block in story_blocks:
+            paragraphs = block.find_all("p")
+            for p in paragraphs:
+                story_text += p.text.strip() + " "
+    else:
+        # Überprüfen Sie die Struktur aus dem neuesten Screenshot
+        story_blocks = detail_soup.find_all("div", class_="block-paragraphs")
+        for block in story_blocks:
+            paragraphs = block.find_all("p")
+            for p in paragraphs:
+                story_text += p.text.strip() + " "
+
+    # Wenn die vorherigen Strukturen nicht gefunden wurden, suchen Sie nach der Struktur aus dem ersten Screenshot
+    if not story_text:
+        paragraphs = detail_soup.find_all("p", attrs={"data-block-key": True})
+        for p in paragraphs:
+            story_text += p.text.strip() + " "
+
+    return story_text.strip()
 
 def scrape_stories(url):
     headers = {'User-Agent': 'Mozilla/5.0 ...'}
-
     try:
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
         stories = []
 
-        # Suche nach allen relevanten Story-Elementen
         for story_div in soup.find_all("div", class_="listblock-item"):
             story_link = story_div.find("a", class_="item-link")
             if not story_link:
@@ -29,27 +50,21 @@ def scrape_stories(url):
             description = story_link.find("p").text.strip() if story_link.find("p") else "Keine Beschreibung"
             detail_link = story_link["href"]
 
-            # Vollständige URL für die Detailanfrage generieren
             detail_url = urljoin(base_url, detail_link)
             parsed_url = urlparse(detail_url)
             if parsed_url.netloc != urlparse(base_url).netloc:
                 continue  # Sicherstellen, dass wir auf der gleichen Domain bleiben
 
-            # Scraping der Detailseite der Story
             detail_response = requests.get(detail_url, headers=headers)
             detail_soup = BeautifulSoup(detail_response.content, 'html.parser')
-            story_blocks = detail_soup.find_all("div", class_="block-paragraphs rich-text")
 
-            story_text = ""
-            for block in story_blocks:
-                paragraphs = block.find_all("p")
-                for p in paragraphs:
-                    story_text += p.text.strip() + " "
+            # Rufen Sie die Funktion auf, um den Text zu scrapen
+            story_text = scrape_story_text(detail_soup)
 
             stories.append({
                 "title": title,
                 "description": description,
-                "text": story_text.strip()
+                "text": story_text
             })
 
         return stories
@@ -57,7 +72,6 @@ def scrape_stories(url):
     except requests.RequestException as e:
         print(f"Error during requests to {url}: {str(e)}")
         return []
-
 
 # URL der Hauptseite der Geschichten
 stories_url = urljoin(base_url, stories_path)
