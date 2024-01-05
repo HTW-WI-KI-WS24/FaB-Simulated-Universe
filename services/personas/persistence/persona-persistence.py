d82c2c52825757e29038c86a453bc05ebfeb8ae7
SpiegelV <leon.schuessler@freenet.de>
2024-01-05 15:26:09 +0100

    Refactor db folder into persona folder

--- services/personas/persistence/persona-persistence.py
+++ services/personas/persistence/persona-persistence.py
@@ -1,7 +1,5 @@
 # app.py
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, jsonify, request, SQLAlchemy, Migrate
 
 #TODO:  Vector Database Implementation
 
